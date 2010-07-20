08480b89035176757e982f7e7c39cdf06f03a69a
Brad Ackerman <brad@facefault.org>
2010-07-19 20:56:40 -0700

    Improved display of kanji (I think).

--- app/helpers/kanji_helper.rb
+++ app/helpers/kanji_helper.rb
@@ -1,6 +1,7 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Xwjdic.helpers do
+  IDEOGRAPHIC_SPACE = "&#x3000;" # HTML entity
 
   # Parse one <character> element.
   def parse_kanjidic_entry(e)
@@ -16,10 +17,20 @@ Xwjdic.helpers do
       readings.push({:reading => reading, :type => type})
     end
     nanori = e.find(".//nanori")
+    nanori_txt = []
     nanori.each do |n|
-      readings.push({:reading => highlight_matches(n), :type => "nanori"})
+      this_reading = highlight_matches(n)
+      readings.push({:reading => this_reading, :type => "nanori"})
+      nanori_txt.push(this_reading)
     end
     res[:readings] = readings
+    
+    # Post-processing readings for the view
+    res[:japanese] = readings.select {|x| x[:type] =~ /^ja_/}.\
+      map {|x| x[:reading] }.join(IDEOGRAPHIC_SPACE)
+    if ! nanori_txt.empty?
+      res[:nanori] = nanori_txt.join(IDEOGRAPHIC_SPACE)
+    end
     
     # Senses
     senses = Array.new

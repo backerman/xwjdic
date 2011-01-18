c2aad9780a4599c9a7fccf9a595c3f58b97dc341
Brad Ackerman <brad@facefault.org>
2011-01-17 17:05:16 -0800

    Display dictionary cross-references.

--- app/helpers/kanji_helper.rb
+++ app/helpers/kanji_helper.rb
@@ -2,6 +2,92 @@
 
 Xwjdic.helpers do
   IDEOGRAPHIC_SPACE = "&#x3000;" # HTML entity
+  
+  # Lookup between dictionary codes in XML and display code/name
+  DICTIONARY_CODE = {
+    "nelson_c" => {
+      :code => "N",
+      :name => "Modern Reader's Japanese-English Character Dictionary " + 
+                "(Classic Nelson)"
+    },
+    "nelson_n" => {
+      :code => "V",
+      :name => "The New Nelson Japanese-English Character Dictionary"
+    },
+    "halpern_njecd" => {
+      :code => "H",
+      :name => "New Japanese-English Character Dictionary (Halpern)"
+    },
+    "halpern_kkld" => {
+      :code => "DK",
+      :name => "Kanji Learners Dictionary (Halpern)"
+    },
+    "heisig" => {
+      :code => "L",
+      :name => "Remembering The Kanji (Heisig)"
+    },
+    "gakken" => {
+      :code => "K",
+      :name => "A New Dictionary of Kanji Usage (Gakken)"
+    },
+    "oneill_names" => {
+      :code => "O",
+      :name => "Japanese Names (O'Neill)"
+    },
+    "oneill_kk" => {
+      :code => "DO",
+      :name => "Essential Kanji (O'Neill)"
+    },
+    "moro" => {
+      # FIXME This one also has the MP element to do
+      :code => "MN",
+      :name => "Daikanwajiten (Morohashi)"
+    },
+    "henshall" => {
+      :code => "E",
+      :name => "A Guide To Remembering Japanese Characters (Henshall)"
+    },
+    "sh_kk" => {
+      :code => "IN",
+      :name => "Kanji & Kana (Spahn and Hadamitzky)"
+    },
+    "sakade" => {
+      :code => "DS",
+      :name => "A Guide To Reading and Writing Japanese (Sakade)"
+    },
+    "henshall3" => {
+      :code => "DH",
+      :name => "A Guide To Reading and Writing Japanese 3ed (Henshall)"
+    },
+    "tutt_cards" => {
+      :code => "DT",
+      :name => "Tuttle Kanji Cards (Kask)"
+    },
+    "crowley" => {
+      :code => "DC",
+      :name => "The Kanji Way to Japanese Language Power (Crowley)"
+    },
+    "kanji_in_context" => {
+      :code => "DJ",
+      :name => "Kanji in Context (Nishiguchi and Kono)"
+    },
+    "busy_people" => {
+      :code => "DB",
+      :name => "Japanese For Busy People (AJLT)"
+    },
+    "kodansha_compact" => {
+      :code => "DG",
+      :name => "Kodansha Compact Kanji Guide"
+    },
+    "jf_cards" => {
+      :code => "DF",
+      :name => "Japanese Kanji Flashcards (Hodges and Okazaki, White Rabbit Press)"
+    },
+    "maniette" => {
+      :code => "DM",
+      :name => "Les Kanjis dans la tête (Maniette)"
+    }
+  }
 
   # Parse one <character> element.
   def parse_kanjidic_entry(e)
@@ -57,6 +143,22 @@ Xwjdic.helpers do
     end
     res[:codepoints] = codepoints
     res[:unicode_hex] = unicode_val
+    
+    # Dictionary references
+    dic_refs = Array.new
+    dic_elems = e.find("./dic_number/dic_ref")
+    dic_elems.each do |elem|
+      dic_lookup = DICTIONARY_CODE[elem.attributes["dr_type"]]
+      if dic_lookup
+        this_dic = dic_lookup.dup
+        this_dic[:index] = highlight_matches(elem)
+        dic_refs.push(this_dic)
+      else
+        logger.debug "Warning: Dictionary code lookup failed for " +
+          "#{elem.attributes["dr_type"].inspect}"
+      end
+    end
+    res[:dic_refs] = dic_refs
     
     res
   end
